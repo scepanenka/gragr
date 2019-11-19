15d9af20018b3f0ec9569220e7d821ffbe1b9690
Aliaksandr Scepanenka <scepanenka@gmail.com>
2019-11-19 17:03:21 +0300

    feat: add organization

---
+++ src/GrAGR.Domain/Entities/Organization.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace GrAGR.Entities
+{
+    public class Organization : AuditedAggregateRoot<int>
+    {
+        public string Name { get; set; }
+
+//        public ICollection<Order> Orders { get; set; }
+    }
+}
